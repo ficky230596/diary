17a5bb3bdf077a62ff02ae8d6c5c11280598db1e
ficky230596 <fickyrahanubun@gmail.com>
2024-03-31 15:22:26 +0800

    terlah modif code saya

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
 from datetime import datetime
 
-connection_string = 'mongodb+srv://Basilius:mario@cluster01.biqbzlj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01'
-client = MongoClient(connection_string)
-db = client.dbficky
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+
+db = client[DB_NAME]
+
+# connection_string = 'mongodb+srv://Basilius:mario@cluster01.biqbzlj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01'
+# client = MongoClient(connection_string)
+# db = client.dbficky
 
 
 app = Flask(__name__)
